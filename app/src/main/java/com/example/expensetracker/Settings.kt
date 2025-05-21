72ef80ca1019cef1eac25469f2d5c56d52fe0dd2
RaviRTirkey <ranjantirkeyravi@gmail.com>
2025-05-21 22:59:15 +0530

    4th Commit

--- app/src/main/java/com/example/expensetracker/Settings.kt
+++ app/src/main/java/com/example/expensetracker/Settings.kt
@@ -1,12 +1,13 @@
 package com.example.expensetracker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.databinding.FragmentSettingsBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -25,20 +26,30 @@ class Settings : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val user = Firebase.auth.currentUser
         val email = user?.email ?: "Unknown User"
 
-        binding.usernameTextView.text = "User Email: ${email}"
+        binding.usernameTextView.text = "User Email: $email"
 
         binding.logoutButton.setOnClickListener {
             Firebase.auth.signOut()
-            findNavController().navigate(R.id.launchScreen)
 
             SharedPreferencesManager().clearLoginState(requireContext())
-            Toast.makeText(requireContext(), "Logged Out", Toast.LENGTH_SHORT).show()
+
+            val restartIntent = requireActivity().packageManager
+                .getLaunchIntentForPackage(requireActivity().packageName)
+
+            if (restartIntent != null) {
+                restartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(restartIntent)
+                requireActivity().finish()
+            } else {
+                Toast.makeText(requireContext(), "Error restarting app", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
